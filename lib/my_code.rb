ee493b9af18ceb1d6e61a2c4e7c9699fbcaae948
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-13 02:59:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,13 +19,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end  
 
 def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length do
-    if source_array == false
-      p false
-      i +=1
-    end   
-      true
+source_array.all?
 end
 
 def reduce_to_any_true(source_array)
